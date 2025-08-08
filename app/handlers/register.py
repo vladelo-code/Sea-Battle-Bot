aa961f5d47c5f98f4b333c99b19ccc4f5e8eb111
vladelo <vlad.lakhtion@gmail.com>
2025-08-08 13:36:11 +0300

    Refactor: small fixes

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -3,7 +3,12 @@ from aiogram import Dispatcher
 from app.handlers import base, stats, game, matchmaking
 
 
-def register_handlers(dp: Dispatcher):
+def register_handlers(dp: Dispatcher) -> None:
+    """
+    Регистрирует все обработчики команд и сообщений для бота.
+
+    :param dp: Экземпляр Dispatcher из aiogram.
+    """
     base.register_handler(dp)
     stats.register_handler(dp)
     game.register_handler(dp)
