3bbbe23a1c75afbf1da47891bcd250c396210555
vladelo <vlad.lakhtion@gmail.com>
2025-10-13 23:26:21 +0300

    6.0.2: fix auto-updating username

--- app/db_utils/player.py
+++ app/db_utils/player.py
@@ -32,7 +32,8 @@ def get_or_create_player(db: Session, telegram_id: str, username: str | None = N
     if player:
         # Обновляем дату последнего появления и, при необходимости, username
         player.last_seen = datetime.now(MOSCOW_TZ)
-        if player.username != username:
+
+        if not username and player.username != username:
             player.username = username
     else:
         player = Player(
